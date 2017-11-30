9d6669116f8ceaba39fb74c220c1e141b7a467d0
admin <dangtinhtong@gmail.com>
2017-11-30 14:43:13 +0700

    First commit

---
+++ Prog7.4/Fraction.m
@@ -0,0 +1,54 @@
+//
+//  Fraction.m
+//  Prog7.4
+//
+//  Created by admin on 11/30/17.
+//  Copyright © 2017 admin. All rights reserved.
+//
+
+#import "Fraction.h"
+
+@implementation Fraction
+@synthesize numerator,denominator;
+-(void) print
+
+{
+    NSLog(@"%i/%i",numerator,denominator);
+}
+
+-(void) setTo:(int)n over:(int)d
+{
+    numerator=n;
+    denominator=d;
+}
+-(double) convertToNum
+{
+    if(denominator!=0)
+        return (double) numerator/denominator;
+    else
+        return NAN;
+}
+
+-(void) add: (Fraction*)f
+{
+    numerator = numerator*f.denominator+denominator*f.numerator;
+    denominator= denominator*f.denominator;
+    
+}
+
+-(void) reduce
+{
+    int u=numerator;
+    int v=denominator;
+    int temp;
+    while(v!=0)
+    {
+        temp=u%v;
+        u=v;
+        v=temp;
+        
+    }
+    numerator/=u;
+    denominator/=u;
+}
+@end
--- Prog7.4/main.m
+++ Prog7.4/main.m
@@ -7,11 +7,30 @@
 //
 
 #import <Foundation/Foundation.h>
-
+#import "Fraction.h"
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        //NSLog(@"Hello, World!");
+        
+        Fraction *myFractionA = [[Fraction alloc] init];
+        Fraction *myFractionB = [[Fraction alloc] init];
+        
+        // Set 1st fraction to 1/4
+        [myFractionA setTo:1 over:4];
+        
+        // Set 2st fraction 1/3
+        [myFractionB setTo:1 over:3];
+        
+        [myFractionA print];
+        NSLog(@"+");
+        [myFractionB print];
+        NSLog(@"=");
+        [myFractionA add:myFractionB];
+        // Reduce the result of the addition and print the result
+        [myFractionB reduce];
+        [myFractionA print];
+        
     }
     return 0;
 }
